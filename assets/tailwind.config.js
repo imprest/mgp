0e34dcb9f3073875ebe65987ff4673062061c6d4
Hardik Varia <hardikvaria@gmail.com>
2021-02-02 08:14:33 +0000

    Some full bleed grid layout progress

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['BlinkMacSystemFont', '-apple-system', "Segoe UI", "Roboto",
+  "Oxygen", "Ubuntu", "Droid Sans", "Fira Sans", "Helvetica Neue", "Helvetica",
+  "Arial", 'sans-serif'],
+    },
     extend: {},
   },
   variants: {
