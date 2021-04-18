7dee97a3b568a92580ffbef20a64514ec253251b
Hardik Varia <hardikvaria@gmail.com>
2021-04-18 00:43:43 +0000

    Working bank reconcilation code

--- lib/mgp/sync/ledger.ex
+++ lib/mgp/sync/ledger.ex
@@ -2,16 +2,164 @@ defmodule Mgp.Sync.Ledger do
   alias Mgp.Sync.DbaseParser
   alias Mgp.Utils
 
-  @zero Decimal.new("0")
+  def run() do
+    code = "MB"
 
-  def run(code) do
-    [
-      "/home/hvaria/backup/MGP20/FIT2103.dbf"
-    ]
-    |> Enum.flat_map(fn x -> parse_dbf(x, code) end)
+    {bank, program} =
+      [
+        "/home/hvaria/backup/MGP20/FIT2103.dbf"
+      ]
+      |> Enum.flat_map(fn x -> parse_dbf(x, code) end)
+      |> combine_cash_splits
+      # |> Enum.sort_by(& &1.date, Date)
+      |> Enum.into(%{}, fn x -> {x.id, x} end)
+      |> compare_entries(bank_csv("/home/hvaria/backup/202103.csv"), [])
 
-    # |> compress({"desc", @zero}, [])
-    # |> csv
+    {rm_contras(bank), csv(Map.to_list(program))}
+  end
+
+  defp csv(map) do
+    map
+    |> Enum.into([], fn {_id, x} -> x end)
+    |> Enum.sort_by(& &1.date, Date)
+    |> Enum.map(fn x ->
+      if x.drcr === "D" do
+        [x.date, "#{x.desc} #{x.post_desc}", x.amount, ""]
+      else
+        [x.date, "#{x.desc} #{x.post_desc}", "", x.amount]
+      end
+
+      # IO.puts([
+      #   Calendar.strftime(x.date, "%d-%m-%Y"),
+      #   ",\"",
+      #   x.ref,
+      #   "\",\"",
+      #   x.desc,
+      #   "\",",
+      #   Decimal.to_string(x.debit),
+      #   ",",
+      #   Decimal.to_string(x.credit)
+      # ])
+    end)
+  end
+
+  defp rm_contras(bank) do
+    rm_contras(bank, [])
+  end
+
+  defp rm_contras([], acc), do: :lists.reverse(acc)
+
+  defp rm_contras([h | t], acc) do
+    [_, _, debit, credit] = h
+
+    result =
+      if debit === "" do
+        find_contra(2, credit, t, false, [])
+      else
+        find_contra(3, debit, t, false, [])
+      end
+
+    case result do
+      {true, new_list} -> rm_contras(new_list, acc)
+      {false, _} -> rm_contras(t, [h | acc])
+    end
+  end
+
+  defp find_contra(_position, _amount, [], false, acc), do: {false, :lists.reverse(acc)}
+
+  defp find_contra(_position, _amount, _, true, acc), do: {true, acc}
+
+  defp find_contra(position, amount, [h | t], found, acc) do
+    if Enum.at(h, position) !== "" and Decimal.compare(Enum.at(h, position), amount) === :eq do
+      find_contra(position, amount, t, true, :lists.reverse(acc) ++ t)
+    else
+      find_contra(position, amount, t, found, [h | acc])
+    end
+  end
+
+  defp combine_cash_splits(p) do
+    Enum.group_by(p, & &1.tx_code)
+    |> Enum.map(fn {id, [h | t] = x} ->
+      # only combine BR 'Bank Receipts like cash that was split into multiple entries'
+      # so that it matches bank csv statement amounts
+      if String.starts_with?(id, "BR R") do
+        if length(x) !== 1 do
+          Enum.reduce(t, h, fn y, acc -> %{acc | amount: Decimal.add(acc.amount, y.amount)} end)
+        else
+          h
+        end
+      else
+        x
+      end
+    end)
+    |> :lists.flatten()
+  end
+
+  def compare_entries(program, [], result), do: {:lists.reverse(result), program}
+
+  def compare_entries(program, [h | t], result) do
+    [date, _desc, debit, credit] = h
+
+    if debit === "" do
+      # Credit Entry
+      r =
+        Enum.find(program, :not_found, fn {_id, m} ->
+          m.drcr === "C" and Decimal.compare(m.amount, credit) === :eq
+        end)
+
+      if r !== :not_found do
+        {id, _} = r
+        compare_entries(Map.delete(program, id), t, result)
+      else
+        compare_entries(program, t, [h | result])
+      end
+    else
+      # Debit Entry
+      r =
+        Enum.find(program, :not_found, fn {_id, m} ->
+          m.drcr === "D" and Decimal.compare(m.amount, debit) === :eq and
+            Date.compare(date, m.date) !== :lt
+        end)
+
+      if r !== :not_found do
+        {id, _} = r
+        compare_entries(Map.delete(program, id), t, result)
+      else
+        compare_entries(program, t, [h | result])
+      end
+    end
+  end
+
+  def bank_csv(file) do
+    File.read!(file)
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(fn x ->
+      [date, desc, _value, debit, credit, _bal] = String.split(x, ",")
+      [bank_date(date), String.slice(desc, 69, 40), debit, credit]
+    end)
+  end
+
+  defp bank_date(date) do
+    [d, m, y] = String.split(date, " ")
+    Date.new!(String.to_integer("20" <> y), month_to_num(m), String.to_integer(d))
+  end
+
+  defp month_to_num(m) do
+    case String.upcase(m) do
+      "JAN" -> 1
+      "FEB" -> 2
+      "MAR" -> 3
+      "APR" -> 4
+      "MAY" -> 5
+      "JUN" -> 6
+      "JUL" -> 7
+      "AUG" -> 8
+      "SEP" -> 9
+      "OCT" -> 10
+      "NOV" -> 11
+      "DEC" -> 12
+    end
   end
 
   def parse_dbf(dbf, code) do
@@ -58,75 +206,5 @@ defmodule Mgp.Sync.Ledger do
         end
       end
     )
-  end
-
-  defp compress([], {_desc, _value}, output) do
-    Enum.reverse(output)
-  end
-
-  defp compress(data, {desc, value}, output) do
-    [h | t] = data
-
-    case t do
-      [] ->
-        case Decimal.compare(value, @zero) do
-          :eq ->
-            compress(t, {desc, value}, [h | output])
-
-          _ ->
-            h = Map.put(h, :desc, desc)
-
-            h =
-              if h.drcr == "D" do
-                Map.put(h, :credit, Decimal.add(value, h.credit))
-              else
-                Map.put(h, :debit, Decimal.add(value, h.debit))
-              end
-
-            compress([], {"desc", @zero}, [h | output])
-        end
-
-      _ ->
-        [next | _] = t
-
-        if h.tx_no == next.tx_no do
-          compress(t, {h.desc, Decimal.add(value, Decimal.add(h.debit, h.credit))}, output)
-        else
-          case Decimal.compare(value, @zero) do
-            :eq ->
-              compress(t, {desc, value}, [h | output])
-
-            _ ->
-              h = Map.put(h, :desc, desc)
-
-              h =
-                if h.drcr == "D" do
-                  Map.put(h, :credit, Decimal.add(value, h.credit))
-                else
-                  Map.put(h, :debit, Decimal.add(value, h.debit))
-                end
-
-              compress(t, {"desc", @zero}, [h | output])
-          end
-        end
-    end
-  end
-
-  defp csv(d) do
-    d
-    |> Enum.sort_by(fn x -> x.date end)
-    |> Enum.map(fn x ->
-      IO.puts([
-        Calendar.strftime(x.date, "%d-%m-%Y"),
-        ",\"",
-        x.ref,
-        "\",\"",
-        x.desc,
-        "\",",
-        Decimal.to_string(x.debit),
-        ",",
-        Decimal.to_string(x.credit)
-      ])
-    end)
   end
 end
